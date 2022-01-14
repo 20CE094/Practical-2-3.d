e69e9eb7011bd9645577f867e2ff450888776874
20CE094 <97758311+20CE094@users.noreply.github.com>
2022-01-15 01:00:39 +0530

    Add files via upload

---
+++ pratical2_3_d.py
@@ -0,0 +1,9 @@
+#Name-Nehal Patel
+#ID- 20CE094
+#Github link: https://github.com/20CE094/Practical-2-3.d.git
+#Aim:d. Write a Python program to find maximum and the minimum value in a set.
+s={1,2,3,4,5,10,12,13}
+#finding maximum value in the set
+print(max(s))
+#finding minimum value in the set
+print(min(s))
